7b49e816d22531cabc5fe782f7d703cb5663557c
Christian Scott <christian@keativitet.no>
2019-09-04 17:51:56 +0200

    Midtveis ca...

--- 06-Struct/01-Veksel.c
+++ 06-Struct/01-Veksel.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+struct Skuff {
+    int antall;
+    int verdi;
+    char* navn;
+};
+
 int main()
 {
+    struct Skuff skuff;
+
+    skuff.antall = 5;
+    skuff.verdi = 10;
+    skuff.navn = "10-ere";
+
+    printf("%s skuffen har %d enheter i seg med verdi %d\n",
+        skuff.navn, skuff.antall, skuff.verdi);
+
+    struct Skuff skuffer[2] = {
+        { 5, 10, "Femmere" },
+        { 9, 1, "Kroner" }
+    };
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%s skuffen har %d enheter i seg med verdi %d\n",
+        skuffer[i].navn, skuffer[i].antall, skuffer[i].verdi);
+    }
+
     return 0;
 }
